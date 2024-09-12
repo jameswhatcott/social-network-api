cddc71caecc414f9b03450333ee7e0a0ab48fcb2
jameswhatcott <james@whatcott.com>
2024-09-11 21:27:42 -0500

    further updates to userroutes to include friend additions

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,10 +1,21 @@
 const router = require('express').Router();
-const { getUsers, getUserById, createUser, updateUser, deleteUser  } = require('../../controllers/users');
+const { 
+    getUsers, 
+    getUserById, 
+    createUser, 
+    updateUser, 
+    deleteUser,
+    addFriend,
+    removeFriend
+    
+} = require('../../controllers/users');
 
 router.route('/').get(getUsers);  // Example route to get all users
-router.route('/:id').get(getUserById);  // Example route to get a user by ID
+router.route('/:userId').get(getUserById);  // Example route to get a user by ID
 router.route('/').post(createUser); // Route to create a new user
-router.route('/:id').put(updateUser); // Route to update a user
-router.route('/:id').delete(deleteUser); // Route to delete a user
+router.route('/:userId').put(updateUser); // Route to update a user
+router.route('/:userId').delete(deleteUser); // Route to delete a user
+router.route('/:userId/friends/:friendId').post(addFriend); // Route to add a friend to a user
+router.route('/:userId/friends/:friendId').delete(removeFriend); // Route to remove friend from a user
 
 module.exports = router;  // Export the router, not an object
