1b0698e3892641b102c572a04433c82b8f91290e
jameswhatcott <james@whatcott.com>
2025-06-24 21:30:40 -0500

    made a route endpoing

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');  // This line is likely causing the issue
 
+// Add a root route
+router.get('/', (req, res) => {
+  res.json({ message: 'Social Media API is running!' });
+});
+
 router.use('/api', apiRoutes);
 
 module.exports = router;
